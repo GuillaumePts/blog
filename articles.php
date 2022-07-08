01778b9b4c8cba56319a4230565041c36031e3ad
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-08 15:40:02 +0200

    rectification le trie est maintenant dans la requete , suppression de la condition dans le foreach manque toujour le css

--- articles.php
+++ articles.php
@@ -3,7 +3,9 @@
 require('./inc/function.php');
 require('./inc/pdo.php');
 
-$select_articles = "SELECT * FROM articles ORDER BY created_at DESC";
+
+
+$select_articles = "SELECT * FROM articles WHERE status = 'publish'  ORDER BY created_at DESC";
 $query = $pdo->prepare($select_articles);
 $query->execute();
 $articles = $query->fetchAll();
@@ -13,16 +15,16 @@ $articles = $query->fetchAll();
 <?php include ('./inc/header.php'); ?>
 
 <h2>Listes des articles</h2>
-<?php foreach ($articles as $article) { 
-        if($article['status']==='publish'){?>
+<?php foreach ($articles as $article) { ?>
+        
 
 <div id="card">
     <p class="fondnoir"><?=$article['title']?></p>
     <p class="fondnoir"><?=$article['auteur']?></p>
     <p class="fondnoir"><?=$article['created_at']?></p>
 </div>
-<?php }
-    }; ?>
+ <?php  }
+  
 
 
 
@@ -32,4 +34,4 @@ $articles = $query->fetchAll();
 
 
 
-<?php include ('./inc/footer.php'); ?>
+ include ('./inc/footer.php'); ?>
